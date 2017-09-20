f2aad67d3cf3f6eab12686eabe5aa68b5d5ab2a9
Mackoyokcam <mark.hangarter@gmail.com>
2017-09-19 23:03:18 -0700

    MVP, can't return 400 for bad get request

--- lab-mark/lib/request-parser.js
+++ lab-mark/lib/request-parser.js
@@ -8,14 +8,14 @@ module.exports = (req) => {
   return new Promise((resolve, reject) => {
     req.url = url.parse(req.url);
     req.url.query = queryString.parse(req.url.query);
-    console.log(req.url.query);
 
+    // Resolve if GET
     if(!(req.method === 'POST' || req.method === 'PUT'))
       return resolve(req);
 
     req.body = {};
     let content = '';
-    // ONLY PARSER THE POST OR PUT REQUEST BODYS
+
     req.on('data', (buffer) => {
       content += buffer.toString();
     });
